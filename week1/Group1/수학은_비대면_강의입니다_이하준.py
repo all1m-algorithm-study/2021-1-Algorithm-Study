1588470e18488f90ccbb65511646245c8eabe6fe
slumbone <59657315+slumbone@users.noreply.github.com>
2021-04-03 18:13:16 +0900

    Create 수학은_비대면_강의입니다_이하준.py

---
+++ week1/Group1/수학은_비대면_강의입니다_이하준.py
@@ -0,0 +1,10 @@
+k=0
+a, b, c, d, e, f = map(int,input().split())
+for x in range(-999,1000):
+    for y in range(-999,1000):
+        if a*x+b*y==c and d*x+e*y==f:
+            print(x,y)
+            k=1
+            break
+    if k==1:
+        break
