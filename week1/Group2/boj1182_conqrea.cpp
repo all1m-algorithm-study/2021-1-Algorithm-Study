8caca0f74855fecafb30b45a453e074044e7d90c
Conqrea <h_e_x@naver.com>
2021-04-04 23:42:03 +0900

    conqrea 1182번 문제 풀이

---
+++ week1/Group2/boj1182_conqrea.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int cnt = 0, n , s;
+int num[21] = { 0, };
+bool v[21];
+
+void SumOfArr(int d, int sum, int lastI) {
+	if (sum == s && d > 0) {
+		cnt++;
+	}
+	if (d >= n || lastI == n)
+		return;
+	for(int i = lastI + 1; i <= n; i++){
+		v[i] = true;
+		SumOfArr(d + 1, sum + num[i], i);
+		v[i] = false;
+	}
+}
+
+int main()
+{
+	cin >> n >> s;
+	for (int i = 1; i <= n; i++)
+		cin >> num[i];
+	SumOfArr(0, 0, 0);
+	cout << cnt;
+}
