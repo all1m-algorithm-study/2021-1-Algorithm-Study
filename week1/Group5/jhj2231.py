4b1573b3ea5400f6e06662d6ed953a1fd070cd61
sumb-10 <saku07891@gmail.com>
2021-04-04 13:18:28 +0900

    jhj2231

---
+++ week1/Group5/jhj2231.py
@@ -0,0 +1,28 @@
+def decomposite_num(num):
+    answer = num
+    while num > 0:
+        answer += num % 10
+        num //= 10
+
+    return answer
+
+
+def find_creator():
+
+    n = int(input())
+    digit_n = int(len(str(n))) #n 자릿수 구하기
+
+    if n < 18:
+        for test_num in range(1, n):
+            if decomposite_num(test_num) == n:
+                return test_num
+        return 0
+
+    else:
+        for test_num in range(n - 9*digit_n, n):
+            if decomposite_num(test_num) == n:
+                return test_num
+        return 0
+
+
+print(find_creator())
