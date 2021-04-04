5c25a6a486115980a1677c07e4ce1c27cbdc1c3f
moran991231 <55349869+moran991231@users.noreply.github.com>
2021-04-04 14:29:38 +0900

    week1 group7 moran991231's solution

---
+++ week1/Group7/Boj_1254_moran991231.java
@@ -0,0 +1,30 @@
+
+import java.io.*;
+
+class Boj_1254 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+	static int stoi(String s) {
+		return Integer.parseInt(s);
+	}
+	static boolean isPalindrom(char[] str, int len, int x) {
+		for(int i=x; i<(len+x)/2; i++)
+			if(str[i] != str[len+x-1-i]) return false;
+		return true;
+	}
+
+	public static void main(String[] args) throws IOException{
+		char[] str = br.readLine().toCharArray();
+		
+		int strLen = str.length;
+		for(int x=0; x<=strLen; x++	) {
+			if(isPalindrom(str, strLen,x)) {
+				System.out.println(strLen+x);
+				break;
+			}
+				
+		}
+		
+	}
+
+}
