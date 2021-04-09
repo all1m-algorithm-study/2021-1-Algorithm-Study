0a817bcab7ae95fdf2e6e9e325fd1514576b68da
hyun gyu <gusrb3164@gmail.com>
2021-04-09 19:34:44 +0900

    feat: add 15093_gusrb3164.py

--- week2/Group1/15903_gusrb3164.py
+++ week2/Group1/15903_gusrb3164.py
@@ -2,12 +2,12 @@ import heapq
 
 
 def main():
-    n, m = map(int, input().split())
+    _, m = map(int, input().split())
     cards = list(map(int, input().split()))
     hq = []
     for card in cards:
         heapq.heappush(hq, card)
-    for i in range(m):
+    for _ in range(m):
         num1 = heapq.heappop(hq)
         num2 = heapq.heappop(hq)
         heapq.heappush(hq, num1+num2)
