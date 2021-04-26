e1c37f0b8d166789f21e72d449432b68d2083151
Conqrea <h_e_x@naver.com>
2021-04-26 20:43:12 +0900

    conqrea 11399, 2720, 2839

---
+++ week2/Group2/boj2720_conqrea.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    for (int i = 1; i <= T; i++) {
+        int n;
+        int q, d, nk, p;
+        cin >> n;
+        q = n / 25;
+        n %= 25;
+        d = n / 10;
+        n %= 10;
+        nk = n / 5;
+        n %= 5;
+        p = n;
+        cout << q << " " << d << " " << nk << " " << p << endl;
+    }
+}
