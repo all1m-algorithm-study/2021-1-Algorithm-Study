fa4f18bedaed0f37e1c88c3af75bfc8d6d5b0179
zeze1004 <thwjd2717@naver.com>
2021-05-03 16:09:20 +0900

    zeze 연습문제 3주차 풀이

---
+++ week3/Group1/4779-zeze.py
@@ -0,0 +1,14 @@
+import sys
+
+def recursion(num):
+  if num == 1:
+    print('-', end='')
+  else:
+    recursion(num // 3)
+    print(' ' * (num // 3), end='')
+    recursion(num // 3)
+
+# 입력 받기
+for num in sys.stdin:
+  recursion(3 ** int(num))
+  print()
