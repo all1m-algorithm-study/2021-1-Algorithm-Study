0a2b3b779bee51d04ad23f9f82c6abe248544b3a
owl-91391 <ljw91391@naver.com>
2021-05-10 19:05:59 +0900

    4주차 2조 이종원 커밋

--- week3/Group2/boj6603_owl91391.py
+++ week3/Group2/boj6603_owl91391.py
@@ -1,17 +1,16 @@
-def comb (n,m,st,se) :
-    if len(se) == m:
+def comb (st,se) :
+    if len(se) == 6: 
         print(*se)
-    elif st <=(len(s)-1) :
-        se.append(s[st])
-        comb(n,m,st+1,se)
-        se.pop()
-        comb(n,m,st+1,se)
-
+    elif st <=(len(s)-1) : 
+        se.append(s[st]) 
+        comb(st+1,se) 
+        se.pop() 
+        comb(st+1,se)
 while True :
     s = list(map(int,input().split()))
-    if s == [0] :
+    if s == [0] : # 0 입력시 종료
         break
     else :
-        del s[0]
-    comb(s,6,0,[])
+        del s[0] # 0을 입력하지 않은 경우, s[0]은 본문의 k개의 수 이므로 편의상 제거         
+    comb(0,[]) #조합 함수
     print('')
