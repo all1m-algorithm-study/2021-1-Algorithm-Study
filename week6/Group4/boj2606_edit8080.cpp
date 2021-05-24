5fce0ae07321926d495374d2e229d1a3d06b2f2b
Taehee_Lee <tlfla01@gmail.com>
2021-05-24 22:52:28 +0900

    week6 edit8080's algorithm study

---
+++ week6/Group4/boj2606_edit8080.cpp
@@ -0,0 +1,47 @@
+﻿/*
+* 1. 대칭 그래프를 구성한다.
+* 2. 방문한 지점을 체크한다.
+* 3. 기준 노드에서 이어져 있는 간선이 존재하고 방문하지 않았다면 해당 노드를 기준으로 재귀적으로 탐색 함수를 호출한다.
+* 
+* 시간복잡도 = O(V+E) (노드 방문 + 간선 방문)
+*/
+#include <iostream>
+#include <vector>
+
+#define SIZE 101
+
+using namespace std;
+
+void virus(vector<vector<int>> &computers, vector<bool> &visited, int x, int n) {
+	visited[x] = true;
+
+	for (int i = 1; i <= n; i++) {
+		if (computers[x][i] != 0 && !visited[i]) {
+			virus(computers, visited, i, n);
+		}
+	}
+	return;
+}
+
+int main(){
+	int n, t;
+	vector<vector<int>> computers(SIZE, vector<int>(SIZE, 0));
+	vector<bool> visited(SIZE, false);
+	int cnt = 0;
+
+	cin >> n >> t;
+
+	for (int i = 0; i < t; i++) {
+		int x, y;
+		cin >> x >> y;
+		computers[x][y] = 1;
+		computers[y][x] = 1;
+	}
+	
+	virus(computers, visited, 1, n);
+	
+	for (int i = 2; i <= n; i++)
+		if (visited[i]) cnt += 1;
+
+	cout << cnt;
+}
---
+++ week6/Group4/boj5567_edit8080.cpp
@@ -0,0 +1,59 @@
+﻿/*
+* 1. 그래프를 구성한다.
+* 2. BFS를 2번 수행한다.
+* 3. 2번 수행후 방문한 노드의 개수를 센다.
+* 
+* 시간복잡도 = O(V+E) (노드 방문 + 간선 방문)
+*/
+
+#include <iostream>
+#include <vector>
+#include <queue>
+
+#define SIZE 10001
+using namespace std;
+
+int invite(vector<vector<int>> &edge, vector<bool> &visited, int n, int x) {
+	queue<int> q;
+	int ret = 0;
+
+	// BFS 1단계 수행
+	for (int i = 0; i < (int)edge[x].size(); i++) {
+		q.push(edge[x][i]);
+		visited[edge[x][i]] = true;
+	}
+
+	// BFS 2단계 수행
+	while (!q.empty()) {
+		int front = q.front();
+		
+		for (int i = 0; i < (int)edge[front].size(); i++)
+			visited[edge[front][i]] = true;
+		
+		q.pop();
+	}
+
+	for (int i = 2; i <= n; i++)
+		if (visited[i]) ret += 1;
+
+	return ret;
+}
+
+int main(){
+	int n, m;
+	vector<vector<int>> edge;
+	vector<bool> visited(SIZE, false);
+
+	edge.resize(SIZE);
+
+	cin >> n >> m;
+
+	for (int i = 0; i < m; i++) {
+		int x, y;
+		cin >> x >> y;
+		edge[x].push_back(y);
+		edge[y].push_back(x);
+	}
+
+	cout << invite(edge, visited, n, 1);	
+}
